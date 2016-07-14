1ac7d1dc5cb4189af5b64022db8656a982130f49
Matthias Vallentin <vallentin@icir.org>
2016-07-14 08:48:45 -0700

    Refactor flare implementation

--- broker/detail/flare.hh
+++ broker/detail/flare.hh
@@ -1,42 +1,42 @@
 #ifndef BROKER_DETAIL_FLARE_HH
 #define BROKER_DETAIL_FLARE_HH
 
-#include "broker/detail/pipe.hh"
-
 namespace broker {
 namespace detail {
 
-// An object that can be used to signal a "ready" status via a file descriptor
-// that may be integrated with select(), poll(), etc. Though it may be used to
-// signal availability of a resource across threads, both access to that
-// resource and the use of the fire/extinguish functions must be performed in
-// a thread-safe manner in order for that to work correctly.
+/// An object that can be used to signal a "ready" status via a file descriptor
+/// that may be integrated with select(), poll(), etc. Though it may be used to
+/// signal availability of a resource across threads, both access to that
+/// resource and the use of the fire/extinguish functions must be performed in
+/// a thread-safe manner in order for that to work correctly.
 class flare {
 public:
+  /// Constructs a flare by opening a UNIX pipe.
   flare();
 
-  // Retrieves a file descriptor that will become ready if the flare has been
-  // fired and not yet extinguishedd.
-  int fd() const {
-    return p.read_fd();
-  }
+  flare(const flare&) = delete;
+  flare& operator=(const flare&) = delete;
 
-  // Put the object in the "ready" state by writing one byte into the
-  // underlying pipe.
+  /// Retrieves a file descriptor that will become ready if the flare has been
+  /// "fired" and not yet "extinguishedd."
+  int fd() const;
+
+  /// Puts the object in the "ready" state by writing one byte into the
+  /// underlying pipe.
   void fire();
 
-  // Take the object out of the "ready" state by consuming all bytes from the
+  // Takes the object out of the "ready" state by consuming all bytes from the
   // underlying pipe.
   void extinguish();
 
-  // Attempts to consume only one byte from the pipe, potentially leaving the
-  // flare in "ready" state.
-  // Returns `true` if one byte was read successfully from the pipe and false
-  // if the pipe had no data to be read.
+  /// Attempts to consume only one byte from the pipe, potentially leaving the
+  /// flare in "ready" state.
+  /// @returns `true` if one byte was read successfully from the pipe and
+  ///          `false` if the pipe had no data to be read.
   bool extinguish_one();
 
 private:
-  pipe p;
+  int fds_[2];
 };
 
 } // namespace detail
--- src/detail/flare.cc
+++ src/detail/flare.cc
@@ -2,38 +2,49 @@
 #include <fcntl.h>
 #include <unistd.h>
 
+#include <exception>
+
 #include "broker/detail/flare.hh"
 
 namespace broker {
 namespace detail {
 
-flare::flare()
-  : p{FD_CLOEXEC, FD_CLOEXEC, O_NONBLOCK, O_NONBLOCK} {
+flare::flare() {
+  if (::pipe(fds_) == -1)
+    std::terminate();
+  ::fcntl(fds_[0], F_SETFD, ::fcntl(fds_[0], F_GETFD) | FD_CLOEXEC);
+  ::fcntl(fds_[1], F_SETFD, ::fcntl(fds_[1], F_GETFD) | FD_CLOEXEC);
+  ::fcntl(fds_[0], F_SETFL, ::fcntl(fds_[0], F_GETFL) | O_NONBLOCK);
+  ::fcntl(fds_[1], F_SETFL, ::fcntl(fds_[1], F_GETFL) | O_NONBLOCK);
+}
+
+int flare::fd() const {
+  return fds_[0];
 }
 
 void flare::fire() {
   char tmp = 0;
   for (;;) {
-    auto n = ::write(p.write_fd(), &tmp, 1);
+    auto n = ::write(fds_[1], &tmp, 1);
     if (n > 0)
       break; // Success -- wrote a byte to pipe.
     if (n < 0 && errno == EAGAIN)
       break; // Success -- pipe is full and just need at least one byte in it.
-    // Loop because either the byte wasn't written or got EINTR error.
+    // Loop, because either the byte wasn't written or we got EINTR.
   }
 }
 
 void flare::extinguish() {
   char tmp[256];
   for (;;)
-    if (::read(p.read_fd(), tmp, sizeof(tmp)) == -1 && errno == EAGAIN)
+    if (::read(fds_[0], tmp, sizeof(tmp)) == -1 && errno == EAGAIN)
       break; // Pipe is now drained.
 }
 
 bool flare::extinguish_one() {
   char tmp = 0;
   for (;;) {
-    auto n = ::read(p.read_fd(), &tmp, 1);
+    auto n = ::read(fds_[0], &tmp, 1);
     if (n == 1)
       return true; // Read one byte.
     if (n < 0 && errno == EAGAIN)
