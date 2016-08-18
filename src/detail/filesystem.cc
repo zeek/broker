fd89d4250feddcb00c0f3af00c135cc0cebda821
Matthias Vallentin <vallentin@icir.org>
2016-08-18 22:17:34 +0200

    Find OPEN_MAX in a platform-independent way

--- src/detail/filesystem.cc
+++ src/detail/filesystem.cc
@@ -1,7 +1,17 @@
 #include <sys/stat.h>
-#include <sys/syslimits.h>
 #include <ftw.h>
+#include <unistd.h>
 
+#include "broker/config.hh"
+
+#ifdef BROKER_BSD
+#include <sys/syslimits.h>
+#endif
+
+#include <cerrno>
+#include <mutex>
+
+#include "broker/detail/die.hh"
 #include "broker/detail/filesystem.hh"
 
 namespace broker {
@@ -22,6 +32,30 @@ int rm(const char* path, const struct stat*, int, FTW*) {
   return ::remove(path);
 }
 
+std::once_flag openmax_flag;
+
+// Portable solution to retrieve the value of OPEN_MAX.
+// Adapted from: http://stackoverflow.com/a/8225250
+long open_max() {
+#ifdef OPEN_MAX
+  static long openmax = OPEN_MAX;
+#else
+  static long openmax = 0;
+#endif
+  std::call_once(openmax_flag, [&] {
+    if (openmax == 0) {
+      errno = 0;
+      if ((openmax = sysconf(_SC_OPEN_MAX)) < 0) {
+        if (errno == 0)
+          openmax = 256; // Guess a value.
+        else
+          die("open_max: sysconf(_SC_OPEN_MAX)");
+      }
+    }
+  });
+  return openmax;
+}
+
 } // namespace <anonymous>
 
 bool remove_all(const path& p) {
@@ -29,8 +63,8 @@ bool remove_all(const path& p) {
   if (::lstat(p.c_str(), &st) != 0)
     return false;
   if (S_ISDIR(st.st_mode))
-    return ::nftw(p.c_str(), rm, OPEN_MAX, FTW_DEPTH | FTW_PHYS) == 0;
-  else 
+    return ::nftw(p.c_str(), rm, open_max(), FTW_DEPTH | FTW_PHYS) == 0;
+  else
     return ::remove(p.c_str()) == 0;
 }
 
