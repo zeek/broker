cd2d6442e5e8358953e7f2779ebc5b4720f7f1b9
Jon Siwek <jsiwek@corelight.com>
2018-09-11 16:11:10 -0500

    Update pybind11 to v2.2.4 release

--- broker/version.hh
+++ broker/version.hh
@@ -12,7 +12,7 @@ using type = unsigned;
 constexpr type major = 1;
 constexpr type minor = 0;
 constexpr type patch = 1;
-constexpr auto suffix = "-58";
+constexpr auto suffix = "-59";
 
 constexpr type protocol = 1;
 
