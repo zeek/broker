46cd8bbcea83125b5b72a50cfa652f2c5a537b88
Jon Siwek <jsiwek@corelight.com>
2018-10-04 11:05:26 -0500

    Add acknowledgements to README

--- broker/version.hh
+++ broker/version.hh
@@ -12,7 +12,7 @@ using type = unsigned;
 constexpr type major = 1;
 constexpr type minor = 1;
 constexpr type patch = 0;
-constexpr auto suffix = "-6";
+constexpr auto suffix = "-7";
 
 constexpr type protocol = 1;
 
