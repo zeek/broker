9eba2abf057923a820d5151fe0b7d7dadbbd930c
Jon Siwek <jsiwek@corelight.com>
2018-10-05 09:30:16 -0500

    Remove README acknowledgements as missing commits are now in master

--- broker/version.hh
+++ broker/version.hh
@@ -12,7 +12,7 @@ using type = unsigned;
 constexpr type major = 1;
 constexpr type minor = 1;
 constexpr type patch = 0;
-constexpr auto suffix = "-8";
+constexpr auto suffix = "-453";
 
 constexpr type protocol = 1;
 
