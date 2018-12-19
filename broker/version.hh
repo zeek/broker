0e514a7d3100d80e6e1bf481db2f5379456ce09e
Jon Siwek <jsiwek@corelight.com>
2018-12-19 09:56:51 -0600

    Updating CHANGES and VERSION.

--- broker/version.hh
+++ broker/version.hh
@@ -11,8 +11,8 @@ using type = unsigned;
 
 constexpr type major = 1;
 constexpr type minor = 1;
-constexpr type patch = 1;
-constexpr auto suffix = "-10";
+constexpr type patch = 2;
+constexpr auto suffix = "";
 
 constexpr type protocol = 1;
 
