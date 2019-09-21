fbd3fab1a125c9cb5523a6e1872162d92b133d36
Jon Siwek <jsiwek@corelight.com>
2019-09-21 11:00:37 -0700

    Update version to 1.2.1

--- broker/version.hh
+++ broker/version.hh
@@ -11,7 +11,7 @@ using type = unsigned;
 
 constexpr type major = 1;
 constexpr type minor = 2;
-constexpr type patch = 0;
+constexpr type patch = 1;
 constexpr auto suffix = "";
 
 constexpr type protocol = 2;
