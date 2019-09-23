18e8fefdab667e7cf3ababe6d4cad6975590993d
Jon Siwek <jsiwek@corelight.com>
2019-09-23 09:58:08 -0700

    Update embedded CAF to 2nd 0.16.4, fixes a compiler warning

--- broker/version.hh
+++ broker/version.hh
@@ -11,7 +11,7 @@ using type = unsigned;
 
 constexpr type major = 1;
 constexpr type minor = 2;
-constexpr type patch = 2;
+constexpr type patch = 3;
 constexpr auto suffix = "";
 
 constexpr type protocol = 2;
