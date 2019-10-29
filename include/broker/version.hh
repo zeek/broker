1a96c6493f405a2bd8d239e66c669b9895374712
Jon Siwek <jsiwek@corelight.com>
2019-10-28 18:06:57 -0700

    Update README to reflect CMake 3.0+ requirement

--- include/broker/version.hh
+++ include/broker/version.hh
@@ -11,7 +11,7 @@ using type = unsigned;
 constexpr type major = 1;
 constexpr type minor = 2;
 constexpr type patch = 0;
-constexpr auto suffix = "-115";
+constexpr auto suffix = "-117";
 
 constexpr type protocol = 2;
 
