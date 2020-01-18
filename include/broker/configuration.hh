fa952b81b50a68c4ac833bff0baa4efbb3159315
Dominik Charousset <dominik.charousset@corelight.com>
2020-01-18 10:48:48 +0100

    Redesign Broker's configuration setup
    The environment variables BROKER_DEBUG_VERBOSE and BROKER_DEBUG_LEVEL
    overlapped and provided fuzzy semantics for a user. In particular, the
    BROKER_DEBUG_VERBOSE variable removed the component blacklist, thus
    making CAF log output visible, while the BROKER_DEBUG_LEVEL does not.
    The variables also manipulated both console and file output.
    
    This commit replaces the previous variables with
    BROKER_CONSOLE_VERBOSITY and BROKER_FILE_VERBOSITY. Almost always, a
    user only cares about the console output. With that change, we can give
    users a clear guidance:
    
    - set `BROKER_CONSOLE_VERBOSITY=info` for seeing high-level events such
      as new connections, incoming peerings, etc.
    - set `BROKER_CONSOLE_VERBOSITY=debug` for tapping into exchanged
      messages and other low-level events (slow)
    
    When debugging a distributed Broker setup, users can instead use
    BROKER_FILE_VERBOSITY to record a test run and then collect the files
    from all endpoints.
    
    In all cases, Broker now omits CAF output. Advanced features are only
    accessible from the (new) `broker.conf`. Here, experienced users can
    remove the component files, tweak format strings, and so on.
    
    The initialization process now also fixes ordering of the various
    configuration parameters:
    
    - `broker.conf` overrides hard-coded defaults
    - environment variables override `broker.conf` entires
    - command line arguments override environment variables
    
    This behavior is the most intuitive for UNIX users. Previously, a
    configuration file would override the environment variables.
    
    Finally, Broker now throws exceptions on invalid configuration entries
    rather than silently dropping them. Also, the `endpoint` checks whether
    the user asked for a help text and stops immediately instead of carrying
    on normally.

--- include/broker/configuration.hh
+++ include/broker/configuration.hh
@@ -25,15 +25,43 @@ struct broker_options {
   broker_options() {}
 };
 
-/// Provides an execution context for brokers.
+/// Configures an ::endpoint.
+///
+/// The configuration draws user-provided options from three sources (in order):
+/// 1. The file `broker.conf`. Contents of this file override hard-coded
+///    defaults. Broker only scans the current working directory when looking
+///    for this file.
+/// 2. Environment variables. Broker currently recognizes the following
+///    environment variables:
+///    - `BROKER_CONSOLE_VERBOSITY`: enables console output by overriding
+///      `logger.console-verbosity`. Valid values are `trace`, `debug`, `info`,
+///      `warning`, and `error`.
+///    - `BROKER_FILE_VERBOSITY`: enables log file output by overriding
+///      `logger.file-verbosity`.
+///    - `BROKER_RECORDING_DIRECTORY` enables recording of meta data for the
+///      `broker-cluster-benchmark` tool.
+///    - `BROKER_OUTPUT_GENERATOR_FILE_CAP` restricts the number of recorded
+///      messages in recording mode.
+/// 3. Command line arguments (if provided).
+///
+/// As a rule of thumb, set `BROKER_CONSOLE_VERBOSITY` to `info` for getting
+/// output on high-level events such as peerings. If you need to tap
+/// into published messages, set `BROKER_CONSOLE_VERBOSITY` to `debug`. Enabling
+/// debug output will slow down Broker and generates a lot of console output.
+///
+/// Writing to a file instead of printing to the command line can help grepping
+/// through large logs or correlating logs from multiple Broker peers.
 class configuration : public caf::actor_system_config {
 public:
   using super = caf::actor_system_config;
 
   /// Default-constructs a configuration.
-  configuration(broker_options opts = broker_options());
+  configuration();
 
-  /// Constructs a configuration from the command line.
+  /// Constructs a configuration with non-default Broker options.
+  explicit configuration(broker_options opts);
+
+  /// Constructs a configuration from command line arguments.
   configuration(int argc, char** argv);
 
   /// Returns default Broker options and flags.
@@ -47,6 +75,8 @@ public:
   static void add_message_types(caf::actor_system_config& cfg);
 
 private:
+  void init(int argc, char** argv);
+
   broker_options options_;
 };
 
