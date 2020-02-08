54aa0b8656235131006cb56077a5c94af7a63f73
Jon Siwek <jsiwek@corelight.com>
2020-02-08 09:39:45 -0800

    Update embedded CAF to 0.17.4

--- include/broker/version.hh
+++ include/broker/version.hh
@@ -11,7 +11,7 @@ using type = unsigned;
 constexpr type major = 1;
 constexpr type minor = 2;
 constexpr type patch = 0;
-constexpr auto suffix = "-156";
+constexpr auto suffix = "-157";
 
 constexpr type protocol = 2;
 
