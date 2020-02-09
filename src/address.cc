73137f90c2e9a1d758a17488583105b953b29617
Dominik Charousset <dominik@charousset.de>
2020-02-09 14:04:04 +0100

    Reimplement address with caf::ip_address for MSVC
    The implementation of `broker::address` used many  UNIX-specific
    functions as well as functions marked as deprecated/unsafe on MSVC (e.g.
    `sprintf`). Basing the implementation on `caf::ip_address` instead of
    rolling a custom implementation avoids dealing with most issues in the
    first place.

--- src/address.cc
+++ src/address.cc
@@ -1,43 +1,76 @@
-#include <sys/socket.h>
-#include <netinet/in.h>
+#include "broker/address.hh"
 
-#include <arpa/inet.h>
 #include <algorithm>
-#include <cstring>
-#include <cstdlib>
+#include <array>
 #include <cstdint>
 #include <cstdio>
+#include <cstdlib>
+#include <cstring>
 #include <string>
-#include <array>
 
-#include "broker/address.hh"
+#include "broker/config.hh"
 #include "broker/detail/hash.hh"
 
+#include <caf/detail/network_order.hpp>
+
 namespace broker {
+
 namespace {
 
-std::array<uint8_t, 12> v4_mapped_prefix
+static constexpr bool is_little_endian =
+#ifdef BROKER_BIG_ENDIAN
+  false;
+#else
+  true;
+#endif
+
+constexpr std::array<uint8_t, 12> v4_mapped_prefix
   = {{0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0xff, 0xff}};
 
-} // namespace <anonymous>
-
-address::address() {
-  bytes_.fill(0);
+auto to_array(const uint32_t* bytes, address::family fam,
+              address::byte_order order) {
+  static constexpr size_t bytes_size
+    = caf::ip_address::num_bytes / sizeof(uint32_t);
+  using std::make_reverse_iterator;
+  caf::ip_address::array_type result;
+  if (fam == address::family::ipv4) {
+    auto dst = std::copy(v4_mapped_prefix.begin(), v4_mapped_prefix.end(),
+                         result.begin());
+    auto first = reinterpret_cast<const uint8_t*>(bytes);
+    auto last = first + sizeof(uint32_t);
+    if constexpr (is_little_endian) {
+      if (order == address::byte_order::host) {
+        std::copy(make_reverse_iterator(last), make_reverse_iterator(first),
+                  dst);
+        return result;
+      }
+    }
+    std::copy(first, last, dst);
+  } else {
+    if constexpr (is_little_endian) {
+      if (order == address::byte_order::host) {
+        auto dst = result.begin();
+        for (auto iter = bytes; iter != bytes + bytes_size; ++iter) {
+          auto first = reinterpret_cast<const uint8_t*>(iter);
+          auto last = first + sizeof(uint32_t);
+          dst = std::copy(make_reverse_iterator(last),
+                          make_reverse_iterator(first), dst);
+        }
+        return result;
+      }
+    }
+    auto first = reinterpret_cast<const uint8_t*>(bytes);
+    auto last = first + caf::ip_address::num_bytes;
+    std::copy(first, last, result.begin());
+  }
+  return result;
 }
 
-address::address(const uint32_t* bytes, family fam, byte_order order) {
-  if (fam == family::ipv4) {
-    std::copy(v4_mapped_prefix.begin(), v4_mapped_prefix.end(), bytes_.begin());
-    auto p = reinterpret_cast<uint32_t*>(&bytes_[12]);
-    *p = (order == byte_order::host) ? htonl(*bytes) : *bytes;
-  } else {
-    std::copy(bytes, bytes + 4, reinterpret_cast<uint32_t*>(&bytes_));
-    if (order == byte_order::host)
-      for (auto i = 0; i < 4; ++i) {
-        auto p = reinterpret_cast<uint32_t*>(&bytes_[i * 4]);
-        *p = htonl(*p);
-      }
-  }
+} // namespace
+
+address::address(const uint32_t* bytes, family fam, byte_order order)
+  : addr_(to_array(bytes, fam, order)) {
+  // nop
 }
 
 static uint32_t bit_mask32(int bottom_bits) {
@@ -52,70 +85,13 @@ bool address::mask(uint8_t top_bits_to_keep) {
   uint32_t mask[4] = {0xffffffff, 0xffffffff, 0xffffffff, 0xffffffff};
   auto res = std::ldiv(top_bits_to_keep, 32);
   if (res.quot < 4)
-    mask[res.quot] = htonl(mask[res.quot] & ~bit_mask32(32 - res.rem));
+    mask[res.quot] = caf::detail::to_network_order(mask[res.quot]
+                                                   & ~bit_mask32(32 - res.rem));
   for (auto i = res.quot + 1; i < 4; ++i)
     mask[i] = 0;
-  auto p = reinterpret_cast<uint32_t*>(&bytes_);
+  auto p = reinterpret_cast<uint32_t*>(&bytes());
   for (auto i = 0; i < 4; ++i)
     p[i] &= mask[i];
-  return true;
-}
-
-bool address::is_v4() const {
-  return memcmp(&bytes_, &v4_mapped_prefix, 12) == 0;
-}
-
-bool address::is_v6() const {
-  return !is_v4();
-}
-
-std::array<uint8_t, 16>& address::bytes() {
-  return bytes_;
-}
-
-const std::array<uint8_t, 16>& address::bytes() const {
-  return bytes_;
-}
-
-bool operator==(const address& lhs, const address& rhs) {
-  return lhs.bytes_ == rhs.bytes_;
-}
-
-bool operator<(const address& lhs, const address& rhs) {
-  return lhs.bytes_ < rhs.bytes_;
-}
-
-bool convert(const address& a, std::string& str) {
-  char buf[INET6_ADDRSTRLEN];
-  if (a.is_v4()) {
-    if (!inet_ntop(AF_INET, &a.bytes()[12], buf, INET_ADDRSTRLEN))
-      return false;
-  } else {
-    if (!inet_ntop(AF_INET6, &a.bytes(), buf, INET6_ADDRSTRLEN))
-      return false;
-  }
-  str = buf;
-  return true;
-}
-
-bool convert(const std::string& str, address& a) {
-  if (str.find(':') != std::string::npos)
-    return inet_pton(AF_INET6, str.c_str(), &a.bytes_) > 0;
-  // IPv4.
-  std::copy(v4_mapped_prefix.begin(),
-            v4_mapped_prefix.end(),
-            a.bytes_.begin());
-  // Parse the address directly instead of using inet_pton since
-  // some platforms have more sensitive implementations than others
-  // that can't e.g. handle leading zeroes.
-  int b[4];
-  int n = sscanf(str.data(), "%d.%d.%d.%d", b + 0, b + 1, b + 2, b + 3);
-  if (n != 4 || b[0] < 0 || b[1] < 0 || b[2] < 0 || b[3] < 0
-      || b[0] > 255 || b[1] > 255 || b[2] > 255 || b[3] > 255)
-    return false;
-  uint32_t raw = (b[0] << 24) | (b[1] << 16) | (b[2] << 8) | b[3];
-  auto p = reinterpret_cast<uint32_t*>(&a.bytes_[12]);
-  *p = htonl(raw);
   return true;
 }
 
