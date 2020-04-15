107d638bce8b92e28ea38e2ca9f033c215465070
Jon Siwek <jsiwek@corelight.com>
2020-04-15 13:45:13 -0700

    GH-109: fix unusable subscriber.poll() method in Python bindings
    (cherry picked from commit 182d6d19ff9ded182122a301ac10cd3d9d3a8178)

--- broker/version.hh
+++ broker/version.hh
@@ -11,7 +11,7 @@ using type = unsigned;
 
 constexpr type major = 1;
 constexpr type minor = 2;
-constexpr type patch = 7;
+constexpr type patch = 8;
 constexpr auto suffix = "";
 
 constexpr type protocol = 2;
