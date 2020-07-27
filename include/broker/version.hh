5edfc7758fa32a2c77f2596e576a2f7c535b6fd5
Jon Siwek <jsiwek@corelight.com>
2020-07-27 10:28:25 -0700

    Update NEWS

--- include/broker/version.hh
+++ include/broker/version.hh
@@ -11,7 +11,7 @@ using type = unsigned;
 constexpr type major = 1;
 constexpr type minor = 4;
 constexpr type patch = 0;
-constexpr auto suffix = "-dev";
+constexpr auto suffix = "";
 
 constexpr type protocol = 2;
 
