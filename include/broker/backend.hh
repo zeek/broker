e82cb55903a1991ac75542f0d8a2fd57d46133b2
Jon Siwek <jsiwek@corelight.com>
2021-01-11 11:05:53 -0800

    Remove deprecated broker::memory/broker::sqlite enums

--- include/broker/backend.hh
+++ include/broker/backend.hh
@@ -10,7 +10,4 @@ enum class backend : uint8_t {
   sqlite, ///< A SQLite3 backend.
 };
 
-[[deprecated("Use broker::backend::memory.")]] constexpr auto memory = backend::memory;
-[[deprecated("Use broker::backend::sqlite.")]] constexpr auto sqlite = backend::sqlite;
-
 } // namespace broker
