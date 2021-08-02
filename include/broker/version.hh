01591c27524ce180c0fdbd80dcf88c0c962e9466
Tim Wojtulewicz <tim@corelight.com>
2021-08-02 10:56:25 -0700

    Fix version.hh for v2.1

--- include/broker/version.hh
+++ include/broker/version.hh
@@ -9,9 +9,9 @@ namespace version {
 using type = unsigned;
 
 constexpr type major = 2;
-constexpr type minor = 2;
+constexpr type minor = 1;
 constexpr type patch = 0;
-constexpr auto suffix = "-dev";
+constexpr auto suffix = "";
 
 constexpr type protocol = 2;
 
