d6ca593190822c5b4ce10728bdac53f8c7e260be
Tim Wojtulewicz <tim@corelight.com>
2022-03-02 14:56:37 -0700

    Rework CI scripts for Windows

--- ci/windows/build.cmd
+++ ci/windows/build.cmd
@@ -1,6 +1,13 @@
-set PATH=%PATH%;C:\Program Files\CMake\bin
+call "c:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
+
 mkdir build
 cd build
-cmake -A x64 -DOPENSSL_ROOT_DIR="C:\Program Files\OpenSSL-Win64" -DEXTRA_FLAGS=/MP%BROKER_CI_CPUS% .. || exit \b 1
-cmake --build . --target install --config release || exit \b 1
+
+:: All of the relevant binaries here are put in the path by the bat file above
+C:\WINDOWS\system32\cmd.exe /c %SYSTEMROOT%\System32\chcp.com 65001 >NUL && ^
+cmake.exe -G "Ninja" -DCMAKE_BUILD_TYPE:STRING="Debug" ^
+-DCMAKE_C_COMPILER=cl.exe -DCMAKE_CXX_COMPILER=cl.exe -DCMAKE_MAKE_PROGRAM=ninja.exe ^
+-DOPENSSL_ROOT_DIR="C:\Program Files\OpenSSL-Win64" -DEXTRA_FLAGS=/MP%BROKER_CI_CPUS% ..
+
+cmake.exe --build . --target install --config release || exit \b 1
 cd ..
