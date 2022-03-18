23ce3b51416450cb158967af7c9d51efda720787
Tim Wojtulewicz <tim@corelight.com>
2022-03-18 13:01:36 -0700

    Add some comments to the build scripts

--- ci/windows/build.cmd
+++ ci/windows/build.cmd
@@ -1,9 +1,14 @@
+:: Import the visual studio compiler environment into the one running in the
+:: cmd current shell. This path is hard coded to the one on the CI image, but
+:: can be adjusted if running builds locally. Unfortunately, the initial path
+:: isn't in the environment so we have to hardcode the whole path.
 call "c:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 
 mkdir build
 cd build
 
-:: All of the relevant binaries here are put in the path by the bat file above
+:: The configure script doesn't work for Windows but we can call cmake directly
+:: to do the same. Ninja is installed as part of the CI image.
 C:\WINDOWS\system32\cmd.exe /c %SYSTEMROOT%\System32\chcp.com 65001 >NUL && ^
 cmake.exe -G "Ninja" -DCMAKE_BUILD_TYPE:STRING="Debug" ^
 -DCMAKE_C_COMPILER=cl.exe -DCMAKE_CXX_COMPILER=cl.exe -DCMAKE_MAKE_PROGRAM=ninja.exe ^
--- ci/windows/test.cmd
+++ ci/windows/test.cmd
@@ -1,3 +1,4 @@
+:: See build.cmd for documentation on this call.
 call "c:\Program Files (x86)\Microsoft Visual Studio\2022\BuildTools\VC\Auxiliary\Build\vcvarsall.bat" x86_amd64
 
 cd build
