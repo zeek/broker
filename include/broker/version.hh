bea08e89bc92d60ebe2b53e200893a5563f1bfee
Tim Wojtulewicz <tim@corelight.com>
2022-05-04 10:42:37 -0700

    Remove missed usage of previously-removed bro.py

--- include/broker/version.hh
+++ include/broker/version.hh
@@ -10,7 +10,7 @@ using type = unsigned;
 constexpr type major = 2;
 constexpr type minor = 2;
 constexpr type patch = 0;
-constexpr auto suffix = "-267";
+constexpr auto suffix = "-268";
 
 constexpr type protocol = 2;
 
