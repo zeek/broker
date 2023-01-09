3047baa5c0298e242e384485583d54b0bb56415c
Tim Wojtulewicz <tim@corelight.com>
2023-01-09 08:41:10 -0700

    Update CHANGES, VERSION, and NEWS for 2.3.6 release

--- include/broker/version.hh
+++ include/broker/version.hh
@@ -9,8 +9,8 @@ using type = unsigned;
 
 constexpr type major = 2;
 constexpr type minor = 3;
-constexpr type patch = 5;
-constexpr auto suffix = "-6";
+constexpr type patch = 6;
+constexpr auto suffix = "";
 
 constexpr type protocol = 2;
 
