5a31576b42157ae27434b77607b717ae6abd2b4f
Dominik Charousset <dominik.charousset@corelight.com>
2023-01-30 17:37:39 +0100

    Simplify running the clang-tidy CI script locally

--- ci/analyze.sh
+++ ci/analyze.sh
@@ -3,6 +3,19 @@
 set -e
 set -x
 
-CXX=clang++ ./configure --build-type=debug
+# When not running in Cirrus, pick system defaults to allow developers to run
+# this script locally to run clang-tidy.
+if [ -z "${BROKER_CI_CPUS}" ]; then
+  if [ `uname` = "Linux" ]; then
+    BROKER_CI_CPUS=`nproc --all`
+  elif [ `uname` = "Darwin" ]; then
+    BROKER_CI_CPUS=`sysctl -n hw.physicalcpu`
+  else
+    echo "Sorry, OS not detected. Please set BROKER_CI_CPUS manually."
+    exit 1
+  fi
+fi
 
+CXX=clang++ ./configure --build-type=debug
+clang-tidy --version
 run-clang-tidy -p build -j ${BROKER_CI_CPUS} $PWD/src
