3cda5be34b1a23a40d24e5ad336a8594c6fd675e
Jan Grashoefer <jan.grashoefer@corelight.com>
2023-04-28 16:07:05 +0200

    Add event timestamp to python bindings.

--- bindings/python/broker/zeek.py
+++ bindings/python/broker/zeek.py
@@ -7,13 +7,14 @@ except ImportError:
 import broker
 
 class Event(_broker.zeek.Event):
-    def __init__(self, *args):
+    def __init__(self, *args, **kwargs):
         if len(args) == 1 and not isinstance(args[0], str):
             # Parse raw broker message as event.
             _broker.zeek.Event.__init__(self, broker.Data.from_py(args[0]))
         else:
-            # (name, arg1, arg2, ...)
-            _broker.zeek.Event.__init__(self, args[0], broker.Data.from_py(args[1:]))
+            # (name, arg1, arg2, ...[, ts])
+            _broker.zeek.Event.__init__(self, args[0], broker.Data.from_py(args[1:]),
+                kwargs.get("ts"))
 
     def args(self):
         return [broker.Data.to_py(a) for a in _broker.zeek.Event.args(self)]
--- bindings/python/zeek.cpp
+++ bindings/python/zeek.cpp
@@ -8,6 +8,7 @@
 #  pragma GCC diagnostic ignored "-Wpedantic"
 #endif
 #include <pybind11/pybind11.h>
+#include <pybind11/stl.h>
 #ifdef __GNUC__
 #  pragma GCC diagnostic pop
 #endif
@@ -27,9 +28,15 @@ void init_zeek(py::module& m) {
   py::class_<broker::zeek::Event, broker::zeek::Message>(m, "Event")
     .def(py::init(
       [](broker::data data) { return broker::zeek::Event(std::move(data)); }))
-    .def(py::init([](std::string name, broker::data args) {
-      return broker::zeek::Event(std::move(name),
-                                 std::move(broker::get<broker::vector>(args)));
+    .def(py::init([](std::string name, broker::data args,
+                     std::optional<double> ts) {
+      if (ts)
+        return broker::zeek::Event(std::move(name),
+                                   std::move(broker::get<broker::vector>(args)),
+                                   broker::to_timestamp(*ts));
+      else
+        return broker::zeek::Event(
+          std::move(name), std::move(broker::get<broker::vector>(args)));
     }))
     .def("valid",
          [](const broker::zeek::Event& ev) -> bool {
@@ -48,6 +55,23 @@ void init_zeek(py::module& m) {
              throw std::invalid_argument("invalid Event data");
            }
            return ev.name();
+         })
+    .def("timestamp",
+         [](const broker::zeek::Event& ev) -> const std::optional<double> {
+           auto t = broker::zeek::Message::type(ev.as_data());
+           if (t != broker::zeek::Message::Type::Event) {
+             throw std::invalid_argument("invalid Event data/type");
+           }
+           if (!ev.valid()) {
+             throw std::invalid_argument("invalid Event data");
+           }
+           if (auto ev_ts = ev.ts()) {
+             double ts;
+             broker::convert(*ev_ts, ts);
+             return ts;
+           }
+
+           return std::nullopt;
          })
     .def("args", [](const broker::zeek::Event& ev) -> const broker::vector& {
       auto t = broker::zeek::Message::type(ev.as_data());
