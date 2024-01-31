e874b36cf9d1fa9d680bb7e389d9612be01b35c2
Dominik Charousset <dominik.charousset@corelight.com>
2024-01-31 21:05:24 +0100

    Fix empty set access on MSVC

--- src/variant.cc
+++ src/variant.cc
@@ -26,11 +26,28 @@ using const_byte_pointer = const std::byte*;
 
 namespace {
 
-const variant_data::set empty_set_instance;
+template <class Container>
+struct container_storage {
+  broker::detail::monotonic_buffer_resource resource;
+  Container container;
+  container_storage()
+    : container(typename Container::allocator_type{&resource}) {}
+};
 
-const variant_data::table empty_table_instance;
+const variant_data::set* empty_set_instance() {
+  static container_storage<variant_data::set> instance;
+  return &instance.container;
+}
 
-const variant_data::list empty_vector_instance;
+const variant_data::table* empty_table_instance() {
+  static container_storage<variant_data::table> instance;
+  return &instance.container;
+}
+
+const variant_data::list* empty_vector_instance() {
+  static container_storage<variant_data::list> instance;
+  return &instance.container;
+}
 
 } // namespace
 
@@ -50,21 +67,21 @@ variant_set variant::to_set() const noexcept {
   using detail_t = variant_data::set*;
   if (auto ptr = std::get_if<detail_t>(&stl_value()))
     return variant_set{*ptr, envelope_};
-  return variant_set{&detail::empty_set_instance, nullptr};
+  return variant_set{detail::empty_set_instance(), nullptr};
 }
 
 variant_table variant::to_table() const noexcept {
   using detail_t = variant_data::table*;
   if (auto ptr = std::get_if<detail_t>(&stl_value()))
     return variant_table{*ptr, envelope_};
-  return variant_table{&detail::empty_table_instance, nullptr};
+  return variant_table{detail::empty_table_instance(), nullptr};
 }
 
 variant_list variant::to_list() const noexcept {
   using detail_t = variant_data::list*;
   if (auto ptr = std::get_if<detail_t>(&stl_value()))
     return variant_list{*ptr, envelope_};
-  return variant_list{&detail::empty_vector_instance, nullptr};
+  return variant_list{detail::empty_vector_instance(), nullptr};
 }
 
 variant_list variant::to_vector() const noexcept {
