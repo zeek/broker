552285e30daa128a27316f43d3f8d5fd0795f2c6
Dominik Charousset <dominik.charousset@corelight.com>
2024-06-03 17:25:05 +0200

    Use the new logging functions in the store actors

--- libbroker/broker/internal_command.cc
+++ libbroker/broker/internal_command.cc
@@ -1,5 +1,7 @@
 #include "broker/internal_command.hh"
 
+#include "caf/deep_to_string.hpp"
+
 namespace broker {
 
 #define TO_STRING_CASE(name)                                                   \
@@ -15,4 +17,65 @@ std::string to_string(command_tag x) {
       return "???";
   }
 }
+
+void convert(const put_command& x, std::string& str) {
+  str = caf::deep_to_string(x);
+}
+
+void convert(const put_unique_command& x, std::string& str) {
+  str = caf::deep_to_string(x);
+}
+
+void convert(const put_unique_result_command& x, std::string& str) {
+  str = caf::deep_to_string(x);
+}
+
+void convert(const erase_command& x, std::string& str) {
+  str = caf::deep_to_string(x);
+}
+
+void convert(const expire_command& x, std::string& str) {
+  str = caf::deep_to_string(x);
+}
+
+void convert(const add_command& x, std::string& str) {
+  str = caf::deep_to_string(x);
+}
+
+void convert(const subtract_command& x, std::string& str) {
+  str = caf::deep_to_string(x);
+}
+
+void convert(const clear_command& x, std::string& str) {
+  str = caf::deep_to_string(x);
+}
+
+void convert(const attach_writer_command& x, std::string& str) {
+  str = caf::deep_to_string(x);
+}
+
+void convert(const ack_clone_command& x, std::string& str) {
+  str = caf::deep_to_string(x);
+}
+
+void convert(const cumulative_ack_command& x, std::string& str) {
+  str = caf::deep_to_string(x);
+}
+
+void convert(const nack_command& x, std::string& str) {
+  str = caf::deep_to_string(x);
+}
+
+void convert(const keepalive_command& x, std::string& str) {
+  str = caf::deep_to_string(x);
+}
+
+void convert(const retransmit_failed_command& x, std::string& str) {
+  str = caf::deep_to_string(x);
+}
+
+void convert(const internal_command& x, std::string& str) {
+  str = caf::deep_to_string(x);
+}
+
 } // namespace broker
