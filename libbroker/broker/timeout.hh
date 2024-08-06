c1a6745a63eb4373f7b682a737dfd3ced62d73e6
Dominik Charousset <dominik.charousset@corelight.com>
2024-08-06 18:07:36 +0200

    Fix build on FreeBSD

--- libbroker/broker/timeout.hh
+++ libbroker/broker/timeout.hh
@@ -1,19 +1,26 @@
 #pragma once
 
-#include "broker/time.hh"
+#include <chrono>
+#include <cstdint>
+#include <ratio>
 
 namespace broker::timeout {
 
-using std::chrono::milliseconds;
-using std::chrono::seconds;
+/// Represents a timeout in milliseconds with a fixed integer type (unlike
+/// std::chrono::milliseconds).
+using milliseconds = std::chrono::duration<int64_t, std::milli>;
+
+/// Represents a timeout in seconds with a fixed integer type (unlike
+/// std::chrono::seconds).
+using seconds = std::chrono::duration<int64_t>;
 
 /// Timeout when peering between two brokers.
-constexpr auto peer = seconds(10);
+constexpr auto peer = seconds{10};
 
 /// Timeout when subscribing to a topic.
-constexpr auto subscribe = seconds(5);
+constexpr auto subscribe = seconds{5};
 
 /// Timeout for interacting with a data store frontend
-constexpr auto frontend = seconds(10);
+constexpr auto frontend = seconds{10};
 
 } // namespace broker::timeout
