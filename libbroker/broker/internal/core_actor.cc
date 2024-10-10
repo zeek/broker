c6c66458fa9232455ef491bd24223196b57f8ba3
Christian Kreibich <christian@corelight.com>
2024-10-10 13:05:30 -0700

    Provide reason when disconnecting a peer because they're slow

--- libbroker/broker/internal/core_actor.cc
+++ libbroker/broker/internal/core_actor.cc
@@ -872,8 +872,8 @@ caf::error core_actor_state::init_new_peer(endpoint_id peer_id,
       // Disconnect unresponsive peers.
       .on_backpressure_buffer(peer_buffer_size(), peer_overflow_policy())
       .do_on_error([this, ptr, peer_id](const caf::error& what) {
-       BROKER_INFO("remove peer" << peer_id << "due to:" << what);
-        ptr->force_disconnect();
+        BROKER_INFO("remove peer" << peer_id << "due to:" << what);
+        ptr->force_disconnect(to_string(what));
       })
       .as_observable());
   // Push messages received from the peer into the central merge point.
