0f99e5b7cb299c7d9390dfa30df7a3ad4efa7f76
Christian Kreibich <christian@corelight.com>
2025-04-17 13:37:10 -0700

    Extend the event observer for client connections/buffering

--- libbroker/broker/event_observer.cc
+++ libbroker/broker/event_observer.cc
@@ -14,4 +14,15 @@ void event_observer::on_peer_buffer_pull(const endpoint_id&,
 
 void event_observer::on_peer_disconnect(const endpoint_id&, const error&) {}
 
+void event_observer::on_client_connect(const endpoint_id&,
+                                       const network_info&) {}
+
+void event_observer::on_client_buffer_push(const endpoint_id&,
+                                           const data_message&) {}
+
+void event_observer::on_client_buffer_pull(const endpoint_id&,
+                                           const data_message&) {}
+
+void event_observer::on_client_disconnect(const endpoint_id&, const error&) {}
+
 } // namespace broker
