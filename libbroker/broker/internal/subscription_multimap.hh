7954833ff52ef1e60643934e3ace2e8123deb8fb
Dominik Charousset <dominik.charousset@corelight.com>
2025-08-10 13:20:12 +0200

    Add new subscription_multimap class
    Currently, we have one filter per subscriber in the core actor. Then, we
    ask each subscriber individually whether a certain topic matches its
    filter.
    
    The new `subscription_multimap` flips this work flow: we register all
    subscribers at the topics they are interested in. Then, we make a single
    pass over the subscriptions to obtain all matches via the `select`
    member function. The use case we have in Broker is to have a single
    filter that stores all subscribers and then do a single central lookup.
    The runtime cost of `select` depends on the number of subscribed topics,
    not on the number of peers. Hence, we should expect better scalability
    when switching to `subscription_multimap`.

---
+++ libbroker/broker/internal/subscription_multimap.hh
@@ -0,0 +1,85 @@
+#pragma once
+
+#include <algorithm>
+#include <set>
+#include <vector>
+
+namespace broker::internal {
+
+template <class ValueType>
+class subscription_multimap {
+public:
+  using key_type = std::string;
+
+  using value_type = ValueType;
+
+  using value_set = std::set<value_type>;
+
+  bool insert(const key_type& key, const value_type& value) {
+    auto i = std::find_if(entries_.begin(), entries_.end(),
+                          [&key](const auto& e) { return e.first == key; });
+    if (i == entries_.end()) {
+      entries_.emplace_back(key, value_set{value});
+      return true;
+    }
+    return i->second.insert(value).second;
+  }
+
+  size_t purge_value(const value_type& value) {
+    size_t count = 0;
+    for (auto i = entries_.begin(); i != entries_.end();) {
+      auto& [key, values] = *i;
+      if (values.erase(value) > 0) {
+        ++count;
+        if (values.empty()) {
+          i = entries_.erase(i);
+        } else {
+          ++i;
+        }
+      } else {
+        ++i;
+      }
+    }
+    return count;
+  }
+
+  const value_set& at(const key_type& key) const noexcept {
+    auto i = std::find_if(entries_.begin(), entries_.end(),
+                          [&key](const auto& e) { return e.first == key; });
+    if (i != entries_.end()) {
+      return i->second;
+    }
+    return empty_set_;
+  }
+
+  bool contains(const key_type& key) const {
+    return std::any_of(entries_.begin(), entries_.end(),
+                       [&key](const auto& e) { return e.first == key; });
+  }
+
+  /// Selects all values that match the given topic, i.e., appends all values
+  /// from entries that have a key that is a prefix of the given topic.
+  void select(std::string_view topic, std::vector<value_type>& result) const {
+    for (const auto& [key, values] : entries_) {
+      if (topic.starts_with(key)) {
+        result.insert(result.end(), values.begin(), values.end());
+      }
+    }
+    std::sort(result.begin(), result.end());
+    result.erase(std::unique(result.begin(), result.end()), result.end());
+  }
+  std::vector<value_type> select(const key_type& topic) const {
+    std::vector<value_type> result;
+    select(topic, result);
+    return result;
+  }
+
+private:
+  using entry_type = std::pair<key_type, value_set>;
+
+  std::vector<entry_type> entries_;
+
+  value_set empty_set_;
+};
+
+} // namespace broker::internal
